5aea7bd49314d9b212869f90d91ef1fb75b75fa4
Mohammed Naser <MohammedNaser2826@gmail.com>
2026-05-30 18:42:59 +0300

    Clean up CSS boilerplate, update HTML title, fix import comment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import FolderSelector from "./App";
-import './App.css'  // or './index.css' depending on which file you used
+import './App.css'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FolderSelector />
